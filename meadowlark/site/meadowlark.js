5ea2346b783cc4e27fef1a55f3111dd1c20f5a29
feihu1996x <gsx0312@qq.com>
2019-01-01 17:30:26 +0800

    视图和静态文件

--- meadowlark/site/meadowlark.js
+++ meadowlark/site/meadowlark.js
@@ -11,6 +11,11 @@ app.set('port', process.env.PORT || 3000)
 
 // 在Express中，路由和中间件的添加顺序至关重要
 
+// static中间件可以将一个或多个目录指派为包含静态资源的目录
+// 其中的资源不经过任何特殊处理直接发送到客户端
+// 可以在其中放图片、CSS文件、客户端JavaScript文件之类的资源
+app.use('/static', express.static(__dirname + '/public'))
+
 // app.VERB在路由匹配时
 // 默认忽略了大小写或反斜杠
 // 并且在进行匹配时也不考虑查询字符串
